0d4dc40494956e1f53f4db60c13c7a971a700eb0
Jonathan Suenaga <jonathansuenaga@Mac-Air-DMA10.local>
2016-07-15 17:36:14 -0300

    frontend start

---
+++ app/src/main/java/br/com/cdf/luiscsv/Login.java
@@ -0,0 +1,32 @@
+package br.com.cdf.luiscsv;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity  implements View.OnClickListener {
+
+    ImageButton ibLogin;
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ibLogin = (ImageButton)findViewById(R.id.ibLogin);
+        ibLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case(R.id.ibLogin):
+                startActivity(new Intent(this,MainActivity.class));
+        }
+    }
+}
