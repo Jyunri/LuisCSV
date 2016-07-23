5a9407703d9d04887b5f571db24514cba6b368bd
Jyunri <nekojimy@gmail.com>
2016-07-23 18:24:44 -0300

    login complete

--- app/src/main/java/br/com/cdf/luiscsv/Login.java
+++ app/src/main/java/br/com/cdf/luiscsv/Login.java
@@ -1,15 +1,31 @@
 package br.com.cdf.luiscsv;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Login extends AppCompatActivity  implements View.OnClickListener {
 
+    EditText etUsername, etPassword;
     ImageButton ibLogin;
 
     @Override
@@ -19,6 +35,9 @@ public class Login extends AppCompatActivity  implements View.OnClickListener {
 
         ibLogin = (ImageButton)findViewById(R.id.ibLogin);
         ibLogin.setOnClickListener(this);
+
+        etUsername = (EditText)findViewById(R.id.etUsername);
+        etPassword = (EditText)findViewById(R.id.etPassword);
     }
 
     @Override
@@ -26,7 +45,9 @@ public class Login extends AppCompatActivity  implements View.OnClickListener {
         switch (v.getId())
         {
             case(R.id.ibLogin):
-                startActivity(new Intent(this,MainActivity.class));
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                new AsyncLogin().execute(username, password);
         }
     }
 
@@ -34,4 +55,134 @@ public class Login extends AppCompatActivity  implements View.OnClickListener {
     public void onBackPressed() {
         return;
     }
+
+    private class AsyncLogin extends AsyncTask<String,String,String> {
+
+        public static final int CONNECTION_TIMEOUT=10000;
+        public static final int READ_TIMEOUT=15000;
+
+        ProgressDialog pdLoading = new ProgressDialog(Login.this);
+        HttpURLConnection conn;
+        URL url = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            //this method will be running on UI thread
+            pdLoading.setMessage("\tCarregando...");
+            pdLoading.setCancelable(false);
+            pdLoading.show();
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                // Enter URL address where your php file resides
+                url = new URL("http://jsuenaga.esy.es/elavonlogin.php");
+
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(),"Malformed",Toast.LENGTH_SHORT).show();
+                return "exception";
+            }
+            try {
+                // Setup HttpURLConnection class to send and receive data from php and mysql
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(READ_TIMEOUT);
+                conn.setConnectTimeout(CONNECTION_TIMEOUT);
+                conn.setRequestMethod("POST");
+
+                // setDoInput and setDoOutput method depict handling of both send and receive
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                // Append parameters to URL
+                Uri.Builder builder = new Uri.Builder()
+                        .appendQueryParameter("username", params[0])
+                        .appendQueryParameter("password", params[1]);
+                String query = builder.build().getEncodedQuery();
+
+
+                // Open connection for sending data
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(query);
+                writer.flush();
+                writer.close();
+                os.close();
+                conn.connect();
+
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+                Toast.makeText(getApplicationContext(),"e1",Toast.LENGTH_SHORT).show();
+                return "exception";
+            }
+
+            try {
+
+                int response_code = conn.getResponseCode();
+
+                // Check if successful connection made
+                if (response_code == HttpURLConnection.HTTP_OK) {
+
+                    // Read data sent from server
+                    InputStream input = conn.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+
+                    // Pass data to onPostExecute method
+                    return (result.toString());
+
+                } else {
+                    Toast.makeText(getApplicationContext(),"connectionbad",Toast.LENGTH_SHORT).show();
+                    return ("unsuccessful");
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "exception";
+            } finally {
+                conn.disconnect();
+            }
+
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            //this method will be running on UI thread
+
+            pdLoading.dismiss();
+            if (result.contains("success")) {
+                /* Here launching another activity when login successful. If you persist login state
+                use sharedPreferences of Android. and logout button to clear sharedPreferences.
+                 */
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+
+            } else if (result.contains("failure")) {
+
+                // If username and password does not match display a error message
+                Toast.makeText(Login.this, "Usuario ou senha invalida", Toast.LENGTH_LONG).show();
+
+            } else if (result.equalsIgnoreCase("exception") || result.equalsIgnoreCase("unsuccessful")) {
+
+                Toast.makeText(Login.this, "Erro de conexao.", Toast.LENGTH_LONG).show();
+            }
+            else
+                Toast.makeText(Login.this,result,Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
